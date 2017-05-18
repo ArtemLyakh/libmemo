536460b3412dce171a338d015109600883f7e25a
Артем <artemm.lyakh@gmail.com>
2017-05-18 17:11:33 +0300

    Добавил авторизацию с сервером и тестовую страницу

--- Libmemo/Libmemo/Helpers/Settings.cs
+++ Libmemo/Libmemo/Helpers/Settings.cs
@@ -1,5 +1,9 @@
 using Plugin.Settings;
 using Plugin.Settings.Abstractions;
+using System;
+using System.Net;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Libmemo {
     public static class Settings {
@@ -10,7 +14,8 @@ namespace Libmemo {
         }
 
         #region Setting Constants
-        private const string _lastModified = "modified";
+        private const string SERVER_URI = "http://libmemo.com";
+
         private static readonly long _lastModifiedDefault = 0;
 
         private const string _dataUrl = "http://libmemo.com/api/data.php";
@@ -23,15 +28,50 @@ namespace Libmemo {
 
         public static long? LastModified {
             get {
-                long res = AppSettings.GetValueOrDefault<long>(_lastModified, _lastModifiedDefault);
+                long res = AppSettings.GetValueOrDefault<long>("modified", _lastModifiedDefault);
                 return res == 0 ? null : (long?)res;
             }
             set {
                 if (value != null) {
-                    AppSettings.AddOrUpdateValue<long>(_lastModified, (long)value);
+                    AppSettings.AddOrUpdateValue<long>("modified", (long)value);
                 } else {
-                    AppSettings.AddOrUpdateValue<long>(_lastModified, _lastModifiedDefault);
+                    AppSettings.AddOrUpdateValue<long>("modified", _lastModifiedDefault);
                 }
+            }
+        }
+
+
+        public static CookieContainer AuthCookies {
+            get {
+                var str = AppSettings.GetValueOrDefault<string>("authCookies", null);
+                if (string.IsNullOrWhiteSpace(str)) return null;
+                else {
+                    List<Cookie> cookieList = JsonConvert.DeserializeObject<List<Cookie>>(str);
+                    CookieCollection cookieCollection = new CookieCollection();
+                    foreach (var cookie in cookieList) {
+                        cookieCollection.Add(cookie);
+                    }
+
+                    CookieContainer cookieContainer = new CookieContainer();
+                    cookieContainer.Add(new Uri(SERVER_URI), cookieCollection);
+
+                    return cookieContainer;
+                }
+            }
+            set {
+                if (value == null) AppSettings.AddOrUpdateValue<string>("authCookies", null);
+                else {
+                    List<Cookie> cookieList = new List<Cookie>();
+                    foreach (var item in value.GetCookies(new Uri(SERVER_URI))) {
+                        Cookie cookie = (Cookie)item;
+                        cookieList.Add(cookie);
+                    }
+
+                    var str = JsonConvert.SerializeObject(cookieList);
+
+                    AppSettings.AddOrUpdateValue<string>("authCookies", str);
+                }
+
             }
         }
 
