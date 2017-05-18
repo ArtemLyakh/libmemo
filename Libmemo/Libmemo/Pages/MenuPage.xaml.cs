1edf4683fe9986b81e03eff2ed57efc1e14041a9
Артем <artemm.lyakh@gmail.com>
2017-05-18 17:12:31 +0300

    Заменил меню на observable collection

--- Libmemo/Libmemo/Pages/MenuPage.xaml.cs
+++ Libmemo/Libmemo/Pages/MenuPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,28 +14,45 @@ namespace Libmemo {
 
         public ListView ListView { get { return this.listView; } }
 
+
+        public ObservableCollection<MenuPageItem> MenuList { get; set; }
+
+
         public MenuPage() {
             InitializeComponent();
 
-            ListView.ItemsSource = new List<MenuPageItem> {
-                new MenuPageItem {
-                    Title = "Карта",
-                    Text = "карта",
-                    Page = typeof(MapPage)
-                },
-                new MenuPageItem {
-                    Title = "Добавить",
-                    Text = "добавить",
-                    Page = typeof(AddPage)
-                },
-                new MenuPageItem {
-                    Title = "test",
-                    Action = () => {
-                        Device.BeginInvokeOnMainThread(async () => {
-                            await App.Current.MainPage.DisplayAlert("Test", "test", "ОК");
-                        });
-                    }
+            MenuList = new ObservableCollection<MenuPageItem>();
+            foreach (var item in GetMenuList()) {
+                MenuList.Add(item);
+            }
+
+            this.BindingContext = this;
+        }
+
+
+
+        private static IEnumerable<MenuPageItem> GetMenuList() {
+            yield return new MenuPageItem {
+                Title = "Карта",
+                Text = "карта",
+                Page = typeof(MapPage)
+            };
+            yield return new MenuPageItem {
+                Title = "Добавить",
+                Text = "добавить",
+                Page = typeof(AddPage)
+            };
+            yield return new MenuPageItem {
+                Title = "test",
+                Action = () => {
+                    Device.BeginInvokeOnMainThread(async () => {
+                        await App.Current.MainPage.DisplayAlert("Test", "test", "ОК");
+                    });
                 }
+            };
+            yield return new MenuPageItem {
+                Title = "AuthTest",
+                Page = typeof(TestAuthPage)
             };
         }
     }
