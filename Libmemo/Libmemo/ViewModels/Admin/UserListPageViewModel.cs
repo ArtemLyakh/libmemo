37b655ccd7ae0180e165e58bc13952f641a7ef95
DESKTOP-RMVCOAF\Артем <artemm.lyakh@gmail.com>
2017-07-14 15:21:42 +0300

    UserListPage

--- Libmemo/Libmemo/ViewModels/Admin/UserListPageViewModel.cs
+++ Libmemo/Libmemo/ViewModels/Admin/UserListPageViewModel.cs
@@ -18,7 +18,11 @@ namespace Libmemo {
             public string Fio { get; set; }
         }
 
-        public UserListPageViewModel() : base() { }
+        public UserListPageViewModel() : base() {
+            this.SearchChanged += (sender, e) => SearchCommand.Execute(null);
+        }
+
+        public ICommand BackCommand => new Command(async () => await App.GlobalPage.Pop());
 
         public ICommand LoadCommand => new Command(async () => await Load());
 
