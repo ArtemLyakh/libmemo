65ebc647d736a892d2908749e65b14638d5e6ed7
DESKTOP-RMVCOAF\Артем <artemm.lyakh@gmail.com>
2017-07-24 14:54:58 +0300

    register

--- Libmemo/Libmemo/ViewModels/Auth/Register/RegisterPageViewModel.cs
+++ Libmemo/Libmemo/ViewModels/Auth/Register/RegisterPageViewModel.cs
@@ -14,11 +14,11 @@ using Xamarin.Forms;
 namespace Libmemo {
     public class RegisterPageViewModel : BaseRegisterViewModel {
 
-        public ICommand LoginCommand {
-            get => new Command(async () => await App.GlobalPage.PushRoot(new LoginPage()));
-        }
+        public RegisterPageViewModel() { }
 
         public ICommand BackCommand => new Command(async () => await App.GlobalPage.Pop());
+
+        public ICommand LoginCommand => new Command(async () => await App.GlobalPage.PushRoot(new LoginPage()));
 
         public class Json {
             public class User {
@@ -32,61 +32,69 @@ namespace Libmemo {
             public PersonJson.Update person { get; set; }
         }
 
-        public ICommand RegisterCommand {
-            get => new Command(async () => {
-                var errors = this.Validate();
-                if (errors.Count() > 0) {
-                    App.ToastNotificator.Show(string.Join("\n", errors));
-                    return;
-                }
+        private bool IsBusy { get; set; } = false;
+        public ICommand RegisterCommand => new Command(async () => {
+            if (IsBusy) return;
 
+            var errors = this.Validate();
+            if (errors.Count() > 0) {
+                App.ToastNotificator.Show(string.Join("\n", errors));
+                return;
+            }
 
-                try {
-                    using (var handler = new HttpClientHandler { CookieContainer = new CookieContainer() })
-                    using (var request = new HttpRequestMessage(HttpMethod.Post, Settings.REGISTER_URL) {
-                        Content = new FormUrlEncodedContent(new Dictionary<string, string> {
-                            {"email", this.Email },
-                            {"password", this.Password },
-                            {"confirm", this.ConfirmPassword }
-                        })
+            App.ToastNotificator.Show("Регистрация");
+            IsBusy = true;
+
+            try {
+                using (var handler = new HttpClientHandler { CookieContainer = new CookieContainer() })
+                using (var request = new HttpRequestMessage(HttpMethod.Post, Settings.REGISTER_URL) {
+                    Content = new FormUrlEncodedContent(new Dictionary<string, string> {
+                        {"email", this.Email },
+                        {"password", this.Password },
+                        {"confirm", this.ConfirmPassword }
                     })
-                    using (HttpClient client = new HttpClient(handler) { Timeout = new TimeSpan(0, 0, 5) })
-                    using (var responce = await client.SendAsync(request)) {
-                        var str = await responce.Content.ReadAsStringAsync();
-                        if (responce.StatusCode == HttpStatusCode.BadRequest) {
-                            var error = JsonConvert.DeserializeObject<JsonMessage>(str).message;
-                            App.ToastNotificator.Show(error);
-                            return;
-                        }
-                        responce.EnsureSuccessStatusCode();
+                })
+                using (HttpClient client = new HttpClient(handler) { Timeout = TimeSpan.FromSeconds(3) })
+                using (var responce = await client.SendAsync(request)) {
+                    var str = await responce.Content.ReadAsStringAsync();
 
-                        var json = JsonConvert.DeserializeObject<Json>(str);
-                        var authInfo = new AuthInfo(
-                            UserId: json.user.id,
-                            IsAdmin: json.user.is_admin,
-                            Email: json.user.email,
-                            Fio: json.user.fio,
-                            CookieContainer: handler.CookieContainer
-                        );
-                        var authCredentials = new AuthCredentials(
-                            Email: Email,
-                            Password: Password
-                        );
-
-                        var person = Person.ConvertFromJson(json.person);
-                        await App.Database.AddPerson(person);
-
-                        AuthHelper.Login(authInfo, authCredentials);
-
-                        App.ToastNotificator.Show("Регистрация успешно завершена");
-                        await App.GlobalPage.PushRoot(new PersonalDataPage());
+                    if (responce.StatusCode == HttpStatusCode.BadRequest) {
+                        var error = JsonConvert.DeserializeObject<JsonMessage>(str).message;
+                        throw new HttpRequestException(error);
                     }
-                } catch {
-                    App.ToastNotificator.Show("Ошибка регистрации");
-                }
-            });
 
-        }
+                    responce.EnsureSuccessStatusCode();
+
+                    var json = JsonConvert.DeserializeObject<Json>(str);
+                    var authInfo = new AuthInfo(
+                        UserId: json.user.id,
+                        IsAdmin: json.user.is_admin,
+                        Email: json.user.email,
+                        Fio: json.user.fio,
+                        CookieContainer: handler.CookieContainer
+                    );
+                    var authCredentials = new AuthCredentials(
+                        Email: Email,
+                        Password: Password
+                    );
+
+                    var person = Person.ConvertFromJson(json.person);
+                    await App.Database.AddPerson(person);
+
+                    AuthHelper.Login(authInfo, authCredentials);
+
+                    await App.GlobalPage.PushRoot(new PersonalDataPage());
+                }
+            } catch (TaskCanceledException) {
+                App.ToastNotificator.Show("Превышен таймаут запроса");
+            } catch (HttpRequestException ex) {
+                App.ToastNotificator.Show(ex.Message);
+            } catch {
+                App.ToastNotificator.Show("Ошибка");
+            } finally {
+                IsBusy = false;
+            }
+        });
 
     }
 }
