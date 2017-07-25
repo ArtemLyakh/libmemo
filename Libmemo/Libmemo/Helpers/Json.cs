e668dced94853d58fdc1c0ef6fb985bc7ea29ebc
DESKTOP-RMVCOAF\Артем <artemm.lyakh@gmail.com>
2017-07-25 09:28:46 +0300

    login page

---
+++ Libmemo/Libmemo/Helpers/Json.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libmemo.Json {
+    public class Auth {
+        public int id { get; set; }
+        public bool is_admin { get; set; }
+        public string fio { get; set; }
+        public string email { get; set; }
+    }
+    public class Message {
+        public string message { get; set; }
+    }
+}
--- Libmemo/Libmemo/Pages/Auth/Login/LoginPage.xaml.cs
+++ Libmemo/Libmemo/Pages/Auth/Login/LoginPage.xaml.cs
@@ -1,19 +1,157 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Libmemo {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage {
+
+        private ViewModel Model {
+            get => (ViewModel)BindingContext;
+            set => BindingContext = value;
+        }
+
         public LoginPage() {
             InitializeComponent();
-            BindingContext = new LoginPageViewModel();
-
         }
+
+        protected override void OnAppearing() {
+            base.OnAppearing();
+            Model = new ViewModel();
+        }
+
+        protected override void OnDisappearing() {
+            base.OnDisappearing();
+            Model.Dispose();
+        }
+
+        protected override bool OnBackButtonPressed() {
+            Device.BeginInvokeOnMainThread(async () => await App.GlobalPage.Pop());
+            return true;
+        }
+
+        public class ViewModel : INotifyPropertyChanged, IDisposable {
+
+            public ViewModel() { }
+
+
+            private string _email = AuthHelper.UserEmail;
+            public string Email {
+                get => this._email;
+                set {
+                    if (this._email != value) {
+                        this._email = value;
+                        this.OnPropertyChanged(nameof(Email));
+                    }
+                }
+            }
+
+            private string _password = AuthHelper.UserPassword;
+            public string Password {
+                get => this._password;
+                set {
+                    if (this._password != value) {
+                        this._password = value;
+                        this.OnPropertyChanged(nameof(Password));
+                    }
+                }
+            }
+
+
+            public ICommand BackCommand => new Command(async () => await App.GlobalPage.Pop());
+
+            private CancellationTokenSource cancelToken { get; set; }
+            private CancellationTokenSource timeoutToken { get; set; }
+            public ICommand LoginCommand => new Command(async () => {
+                if (cancelToken != null) return;
+
+                if (string.IsNullOrWhiteSpace(this.Email)) {
+                    App.ToastNotificator.Show("Не заполнен email");
+                    return;
+                }
+                if (string.IsNullOrWhiteSpace(this.Password)) {
+                    App.ToastNotificator.Show("Не заполнен пароль");
+                    return;
+                }
+
+                HttpResponseMessage responce = null;
+                HttpClientHandler handler = new HttpClientHandler { CookieContainer = new CookieContainer() };
+                try {
+                    App.ToastNotificator.Show("Авторизация");
+                    timeoutToken = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+                    cancelToken = CancellationTokenSource.CreateLinkedTokenSource(timeoutToken.Token);
+
+                    using (var request = new HttpRequestMessage(HttpMethod.Post, Settings.LOGIN_URL) {
+                        Content = new FormUrlEncodedContent(new Dictionary<string, string> {
+                        {"email", this.Email },
+                        {"password", this.Password }
+                    }) })
+                    using (HttpClient client = new HttpClient(handler)) {
+                        responce = await client.SendAsync(request, cancelToken.Token);
+                    }
+                } catch (OperationCanceledException) {
+                    if (timeoutToken.Token.IsCancellationRequested) App.ToastNotificator.Show("Превышен интервал запроса");
+                    return;
+                } finally {
+                    cancelToken = null;
+                    timeoutToken = null;
+                }
+
+                if (responce != null) {
+                    try {
+                        var str = await responce.Content.ReadAsStringAsync();
+
+                        if (responce.StatusCode == HttpStatusCode.BadRequest) {
+                            var error = JsonConvert.DeserializeObject<Json.Message>(str).message;
+                            throw new HttpRequestException(error);
+                        }
+
+                        responce.EnsureSuccessStatusCode();
+
+                        var authJson = JsonConvert.DeserializeObject<Json.Auth>(str);
+                        var authInfo = new AuthInfo(
+                            IsAdmin: authJson.is_admin,
+                            UserId: authJson.id,
+                            Email: authJson.email,
+                            Fio: authJson.fio,
+                            CookieContainer: handler.CookieContainer
+                        );
+                        var authCredentials = new AuthCredentials(
+                            Email: Email,
+                            Password: Password
+                        );
+
+                        AuthHelper.Login(authInfo, authCredentials);
+
+                        await App.GlobalPage.PopToRootPage();
+                    } catch (HttpRequestException ex) {
+                        App.ToastNotificator.Show(ex.Message);
+                    } catch {
+                        App.ToastNotificator.Show("Ошибка");
+                    }
+                }
+            });
+
+            public ICommand RegisterCommand => new Command(async () => await App.GlobalPage.PushRoot(new RegisterPage()));
+
+            public void Dispose() {
+                cancelToken?.Cancel();
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged(string propertyName = "") =>
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
