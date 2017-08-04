094809fd74858dfd8b3adde93df4a5d668a31397
DESKTOP-RMVCOAF\Артем <artemm.lyakh@gmail.com>
2017-08-04 14:50:38 +0300

    treepage global httpclient

--- Libmemo/Libmemo/Pages/TreePage.xaml.cs
+++ Libmemo/Libmemo/Pages/TreePage.xaml.cs
@@ -24,44 +24,8 @@ namespace Libmemo {
 
         protected override void OnDisappearing() {
             base.OnDisappearing();
-            cancelToken?.Cancel();
+            cancelTokenSource?.Cancel();
         }
-
-
-        private CancellationTokenSource cancelToken { get; set; }
-        private CancellationTokenSource timeoutToken { get; set; }
-
-        private async Task<Tree.Json> LoadData() {
-
-            HttpResponseMessage responce = null;            
-            try {
-                timeoutToken = new CancellationTokenSource(TimeSpan.FromSeconds(15));
-                cancelToken = CancellationTokenSource.CreateLinkedTokenSource(timeoutToken.Token);
-
-                using (HttpClientHandler handler = new HttpClientHandler { CookieContainer = AuthHelper.CookieContainer })
-                using (var client = new HttpClient(handler)) {
-                    var timer = System.Diagnostics.Stopwatch.StartNew();
-                    responce = await client.GetAsync(Settings.TREE_DATA_URL, cancelToken.Token);
-                    timer.Stop();
-                    var time = timer.Elapsed;
-                }
-            } finally {
-                cancelToken = null;
-                timeoutToken = null;
-            }
-
-
-            var str = await responce.Content.ReadAsStringAsync();
-
-            if (responce.StatusCode == System.Net.HttpStatusCode.Unauthorized) {
-                throw new UnauthorizedAccessException();
-            }
-            responce.EnsureSuccessStatusCode();
-
-            var json = Newtonsoft.Json.JsonConvert.DeserializeObject<Tree.Json>(str);
-            return json;
-        }
-
 
         private async void Init() {
             if (!AuthHelper.IsLogged || !AuthHelper.CurrentUserId.HasValue) {
@@ -101,16 +65,36 @@ namespace Libmemo {
             }
         });
 
+        private CancellationTokenSource cancelTokenSource { get; set; }
         public ICommand ResetCommand => new Command(async () => {
-            if (cancelToken != null) return;
+            if (cancelTokenSource != null) return;
 
-            Tree.Json data = null;
-
+            HttpResponseMessage responce = null;
             try {
-                data = await LoadData();
-            } catch (OperationCanceledException) {
-                if (timeoutToken.Token.IsCancellationRequested) App.ToastNotificator.Show("Превышен интервал запроса");
+                cancelTokenSource = new CancellationTokenSource();
+                responce = await WebClient.Instance.SendAsync(HttpMethod.Get, new Uri(Settings.TREE_DATA_URL), null, 10, cancelTokenSource.Token);
+            } catch (TimeoutException) {
+                App.ToastNotificator.Show("Превышен интервал запроса");
                 return;
+            } catch (OperationCanceledException) { //cancel
+                return;
+            } catch {
+                App.ToastNotificator.Show("Ошибка");
+                return;
+            } finally {
+                cancelTokenSource = null;
+            }
+
+            Tree.Json json = null;
+            try {
+                var str = await responce.Content.ReadAsStringAsync();
+
+                if (responce.StatusCode == System.Net.HttpStatusCode.Unauthorized) {
+                    throw new UnauthorizedAccessException();
+                }
+                responce.EnsureSuccessStatusCode();
+
+                json = Newtonsoft.Json.JsonConvert.DeserializeObject<Tree.Json>(str);
             } catch (UnauthorizedAccessException) {
                 await AuthHelper.ReloginAsync();
                 return;
@@ -119,7 +103,7 @@ namespace Libmemo {
                 return;
             }
 
-            await Tree.LoadFromJson(data);
+            await Tree.LoadFromJson(json);
             Tree.DrawTree();
         });
 
