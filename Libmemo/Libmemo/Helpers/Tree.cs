0db9c08c41db8c68b4e5cc8f52188ce0458f93c1
Artem <artemm.lyakh@gmail.com>
2017-08-29 15:09:50 +0300

    tree api2 p2

---
+++ Libmemo/Libmemo/Helpers/Tree.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Libmemo.Helpers
+{
+    public class Tree
+    {
+        private AbsoluteLayout Layout { get; set; }
+        private ScrollView Scroll { get; set; }
+        public Item.RootItem Root { get; set; }
+
+		public abstract class Item
+		{
+			public int Id { get; set; }
+			public string Fio { get; set; }
+			public ImageSource Image;
+
+            public Item(int id, string fio, string image_uri = null)
+            {
+                Id = id;
+                Fio = fio;
+                Image = image_uri != null && Uri.TryCreate(image_uri, UriKind.Absolute, out Uri uri)
+                    ? ImageSource.FromUri(uri)
+                    : ImageSource.FromFile("");
+            }
+
+			public abstract class ItemWithRelatives : Item
+			{
+				public ItemWithRelatives(int id, string fio, string image_uri = null) : base(id, fio, image_uri) { }
+
+				public NormalItem Mother { get; set; }
+				public NormalItem Father { get; set; }
+				public List<SiblingItem> Siblings { get; set; } = new List<SiblingItem>();
+			}
+
+			public class SiblingItem : Item
+			{
+				public SiblingItem(int id, string fio, string image_uri = null) : base(id, fio, image_uri) { }
+			}
+
+
+            public class NormalItem : ItemWithRelatives
+            {
+                public NormalItem(int id, string fio, string image_uri = null) : base(id, fio, image_uri) {}
+            }
+
+			public class RootItem : ItemWithRelatives
+			{
+				public RootItem(int id, string fio, string image_uri = null) : base(id, fio, image_uri) { }
+			}
+
+		}
+
+
+        public Tree(AbsoluteLayout absolute, ScrollView scroll, Json.Tree json)
+		{
+			this.Layout = absolute;
+			this.Scroll = scroll;
+		}
+
+
+
+
+
+    }
+}
